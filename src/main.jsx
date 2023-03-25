57c675deccdccd42799cedb7e8c252dcef7d8b45
DragonDev <thanhlongedu0304@gmail.com>
2023-03-25 10:06:00 +0700

    Uninstall babel plugin module resolver & config resolver path

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import './assets/styles/reset.scss';
-import './assets/styles/main.scss';
+import '@/assets/styles/reset.scss';
+import '@/assets/styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
